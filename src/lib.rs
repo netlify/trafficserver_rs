25d770f2b3867ebb99aa0314e6c0133e9637135f
Marcus Weiner <marcus.weiner@gmail.com>
2020-08-23 12:12:03 +0200

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
@@ -14,18 +13,26 @@ pub use remap::*;
 pub fn ts_debug(tag: &str, message: &str) {
     let t = CString::new(tag).unwrap_or_default();
     let s = CString::new(message).unwrap_or_default();
-    unsafe { TSDebug(t.as_ptr(), s.as_ptr()); }
+    unsafe {
+        TSDebug(t.as_ptr(), s.as_ptr());
+    }
 }
 
 pub fn ts_error(message: &str) {
     let s = CString::new(message).unwrap_or_default();
-    unsafe { TSError(s.as_ptr()); }
+    unsafe {
+        TSError(s.as_ptr());
+    }
 }
 
 pub fn ts_config_dir_get() -> &'static str {
-    let dir = unsafe { 
+    let dir = unsafe {
         let dir = TSConfigDirGet();
-        if dir.is_null() { Default::default() } else { CStr::from_ptr(dir) }
+        if dir.is_null() {
+            Default::default()
+        } else {
+            CStr::from_ptr(dir)
+        }
     };
 
     dir.to_str().unwrap_or_default()
